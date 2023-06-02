f0812881272312406ea0771a859d1fda23b95055
fbtjdwls04 <fbtjdwls04@gmail.com>
2023-06-02 19:40:59 +0900

    할 일 form 체크

--- src/pages/WritePage.js
+++ src/pages/WritePage.js
@@ -1,23 +1,49 @@
-import { TextField } from "@mui/material";
+import { TextField, Button } from "@mui/material";
 
 export default function WritePage() {
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const form = e.target;
+    if (form.regDate.value.length == 0) {
+      alert("날짜를 입력해주세요");
+      form.regDate.focus();
+      return;
+    }
+    if (form.content.value.length == 0) {
+      alert("내용을 입력해주세요");
+      form.content.focus();
+      return;
+    }
+  };
+
   return (
     <>
-      <div className="flex-1 flex p-10 flex-col gap-7">
+      <form className="flex-1 flex p-10 flex-col gap-7" onSubmit={onSubmit}>
         <TextField
+          name="regDate"
           id="outlined-basic"
           label="언제 해야하나요?"
-          variant="outlined"
           focused
           type="datetime-local"
         />
         <TextField
-          label="할 일을 입력해주세요"
-          variant="outlined"
+          name="content"
+          label="무엇을 해야하나요?"
           className="flex flex-1"
           multiline
+          InputProps={{ className: "flex-1 flex-col" }}
+          inputProps={{ className: "flex-1" }}
         />
-      </div>
+
+        <Button type="submit" variant="contained">
+          <span>
+            <i class="fa-solid fa-pen"></i>
+          </span>
+          <span>&nbsp;</span>
+          <span>할 일 추가</span>
+        </Button>
+      </form>
     </>
   );
 }
